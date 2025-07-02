aec4457543f11a6f54a4876516f352d3a40513e5
sanket9162 <sanketgondhali55@gmail.com>
2025-07-02 19:18:49 +0530

    database models

--- internal/models/models.go
+++ internal/models/models.go
@@ -30,8 +30,60 @@ type Widget struct {
 	Description    string    `json:"description"`
 	InventoryLevel int       `json:"inventory_level"`
 	Price          int       `json:"price"`
+	Image          string    `json:"image"`
 	CreateAt       time.Time `json:"-"`
 	UpdatedAt      time.Time `json:"-"`
+}
+
+// Order is the type for all orders
+type Order struct {
+	ID            int       `json:"id"`
+	WidgetID      int       `json:"widget_id"`
+	TransactionID int       `json:"transaction_id"`
+	StatusID      int       `json:"status_id"`
+	Quantity      int       `json:"quantity"`
+	Amount        int       `json:"amount"`
+	CreateAt      time.Time `json:"-"`
+	UpdatedAt     time.Time `json:"-"`
+}
+
+// Status is the type for statues
+type Status struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreateAt  time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+// TransactionStatus is the type for transaction statues
+type TransactionStatus struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreateAt  time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
+}
+
+// Transaction is the type for transaction
+type Transaction struct {
+	ID                  int       `json:"id"`
+	Amount              int       `json:"amount"`
+	Currency            string    `json:"currency"`
+	LastFour            string    `json:"last_four"`
+	BankReturnCode      string    `json:"Bank_return_code"`
+	TransactionStatusId int       `json:"Transaction_status_id"`
+	CreateAt            time.Time `json:"-"`
+	UpdatedAt           time.Time `json:"-"`
+}
+
+// User is the type for User
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"First_name"`
+	LastName  string    `json:"Last_Name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreateAt  time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 }
 
 func (m *DBModel) GetWidget(id int) (Widget, error) {
