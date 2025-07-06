62d4655368e84dc17df2a265cdc3efb708213eca
sanket9162 <sanketgondhali55@gmail.com>
2025-07-06 12:23:01 +0530

    virtual terminal

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,6 +12,9 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/", app.Home)
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/virtual-terminal-payment-succeeded", app.VirtualTerminalPaymentSucceeded)
+	mux.Get("/virtual-terminal-receipt", app.VirtualTerminalReceipt)
+
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 	mux.Get("/receipt", app.Receipt)
 
