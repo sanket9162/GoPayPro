3dc73afa540e628d621eaff193c0d616476007c6
sanket9162 <sanketgondhali55@gmail.com>
2025-07-26 10:29:42 +0530

    login page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -289,3 +289,10 @@ func (app *application) BronzePlanReceipt(w http.ResponseWriter, r *http.Request
 		app.errorLog.Print(err)
 	}
 }
+
+// LoginPage diaplays login page
+func (app *application) LoginPage(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "login", &templateData{}); err != nil {
+		app.errorLog.Print(err)
+	}
+}
