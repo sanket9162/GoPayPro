a9d35ac5e493baee116b93a29cbae55af862020b
sanket9162 <sanketgondhali55@gmail.com>
2025-08-16 10:43:41 +0530

    adding and encryption package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -75,7 +75,7 @@ func main() {
 	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
 	flag.StringVar(&cfg.db.dsn, "dsn", "root:database@tcp(localhost:3306)/widgets?parseTime=true&tls=false", "DSN")
 	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
-	flag.StringVar(&cfg.secretkey, "secret", "kln34na9wh4t", "secre tkey")
+	flag.StringVar(&cfg.secretkey, "secret", "DekwoFLEisHeomaLzuqMaMDORdaowlnFw", "secre tkey")
 	flag.StringVar(&cfg.frontend, "frontend", "http://localhost:4000", "url to front end")
 
 	flag.Parse()
