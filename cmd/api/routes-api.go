b6f7b99c6ce80f45dfd42c96cdf55a231a6fe2df
sanket9162 <sanketgondhali55@gmail.com>
2025-08-21 09:45:25 +0530

    api handler and route to process refunds

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -32,6 +32,7 @@ func (app *application) routes() http.Handler {
 		mux.Post("/all-sales", app.AllSales)
 		mux.Post("/get-sale/{id}", app.GetSale)
 		mux.Post("/all-subscriptions", app.AllSubscriptions)
+		mux.Post("/refunc", app.RefundCharge)
 
 	})
 
